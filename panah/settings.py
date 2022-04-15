57f15e25032e3f474fd53ff98a95e1d1e3c69c77
neznour <nazinourollahi@gmail.com>
2022-04-15 13:21:23 -0400

    change pss

--- panah/settings.py
+++ panah/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME':  'test',
         'USER': 'postgres',
-        'PASSWORD': 'Panah!555',
+        'PASSWORD': 'Admin',
         'HOST':'localhost',
         'PORT': '5432',
     
